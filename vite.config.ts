ca927435d7ff5786c10ccbc52bd6070427156155
mioe <mioe.boss@gmail.com>
2023-04-16 10:27:28 +0300

    add esm _dirname

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import Pages from 'vite-plugin-pages'
 
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
 	console.log('🦕 vite.config.ts/defineConfig', command, mode)
